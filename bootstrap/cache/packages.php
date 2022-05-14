f1c6d78494a0ae92ed739651b370605574deb537
Angel <sarrom95@gmail.com>
2022-05-14 15:26:00 +0200

    Se añade react

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -31,6 +31,13 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
   'laraveles/spanish' => 
   array (
     'providers' => 
