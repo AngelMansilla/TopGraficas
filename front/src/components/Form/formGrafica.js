9ee2d28e991ef6eaa9dd558fbfdfe31a75df51a3
Angel Mansilla Puerto <amansill@emeal.nttdata.com>
2022-06-14 10:20:43 +0200

    Validacion formulario grafica

--- front/src/components/Form/formGrafica.js
+++ front/src/components/Form/formGrafica.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import * as moment from 'moment';
 import useGrafica from "../../hooks/useGrafica";
 import { Link } from "wouter";
 import Spinner from "../Spinner";
@@ -21,7 +22,17 @@ export default function FormGrafica({ grafica_id }) {
     postGrafica,
     putGrafica,
   } = useGrafica();
-
+  const [errorNombre, setErrorNombre] = useState("");
+  const [errorEmpresa, setErrorEmpresa] = useState("");
+  const [errorPvpr, setErrorPvpr] = useState("");
+  const [errorArquitectura, setErrorArquitectura] = useState("");
+  const [errorMemoria, setErrorMemoria] = useState("");
+  const [errorTipo_memoria, setErrorTipo_memoria] = useState("");
+  const [errorConsumo, setErrorConsumo] = useState("");
+  const [errorFecha, setErrorFecha] = useState("");
+  const [I, setI] = useState("");
+  const [formValido, setFormValido] = useState(false);
+  const regPvpr = newRegExp("/^[\d]{0,11}(\.[\d]{1,2})?$/")
   useEffect(() => {
     if (grafica_id) {
       getServices({ keyword, id: grafica_id }).then((grafica) => {
@@ -37,10 +48,96 @@ export default function FormGrafica({ grafica_id }) {
     }
   }, [grafica_id]);
 
+  const handleChange = (target) => {
+    if (target.name === "nombre") {
+      if (target.value.lengeht > 3) {
+        setNombre(target.value)
+        setErrorNombre(false)
+      } else {
+        setErrorNombre(true)
+      }
+    }
+    if (target.name === "empresa") {
+      if (target.value.lengeht > 3) {
+        setEmpresa(target.value)
+        setErrorEmpresa(false)
+      } else {
+        setErrorEmpresa(true)
+      }
+    }
+    if (target.name === "pvpr") {
+      if (target.value > 0 && regPvpr.test(target.value)) {
+        setPvpr(target.value)
+        setErrorPvpr(false)
+      } else {
+        setErrorPvpr(true)
+      }
+    }
+    if (target.name === "arquitectura") {
+      if (target.value.lengeht > 3) {
+        setArquitectura(target.value)
+        setErrorArquitectura(false)
+      } else {
+        setErrorArquitectura(true)
+      }
+    }
+    if (target.name === "memoria") {
+      if (target.value.lengeht > 1) {
+        setMemoria(target.value)
+        setErrorMemoria(false)
+      } else {
+        setErrorMemoria(true)
+      }
+    }
+    if (target.name === "tipo_memoria") {
+      if (target.value.lengeht > 1) {
+        setTipo_memoria(target.value)
+        setErrorTipo_memoria(false)
+      } else {
+        setErrorTipo_memoria(true)
+      }
+    }
+    if (target.name === "consumo") {
+      if (target.value.lengeht > 1) {
+        setConsumo(target.value)
+        setErrorConsumo(false)
+      } else {
+        setErrorConsumo(true)
+      }
+    }
+    if (target.name === "fecha") {
+      if (moment(target.value, 'MM/DD/YY', true).isValid()) {
+        setFecha(target.value)
+        setErrorFecha(false)
+      } else {
+        setErrorFecha(true)
+      }
+    }
+    if (target.name === "fecha") {
+      if (target.files[0].name !== "") {
+        setImagen(target.files[0].name)
+        setI(false)
+      } else {
+        setI(true)
+      }
+    }
+    setFormValido(
+      errorArquitectura === false &&
+      errorNombre === false &&
+      errorEmpresa === false &&
+      errorConsumo === false &&
+      errorFecha === false &&
+      errorMemoria === false &&
+      errorTipo_memoria === false &&
+      errorPvpr === false &&
+      errorImagen === false)
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    grafica_id
-      ? putGrafica({
+    if (formValido) {
+      grafica_id
+        ? putGrafica({
           grafica_id,
           nombre,
           empresa,
@@ -52,7 +149,7 @@ export default function FormGrafica({ grafica_id }) {
           fecha,
           imagen,
         })
-      : postGrafica({
+        : postGrafica({
           nombre,
           empresa,
           pvpr,
@@ -64,16 +161,17 @@ export default function FormGrafica({ grafica_id }) {
           imagen,
         });
 
-    if (!grafica_id) {
-      setNombre("");
-      setEmpresa("");
-      setPvpr("");
-      setArquitectura("");
-      setMemoria("");
-      setTipo_memoria("");
-      setConsumo("");
-      setFecha("");
-      setImagen(null);
+      if (!grafica_id) {
+        setNombre("");
+        setEmpresa("");
+        setPvpr("");
+        setArquitectura("");
+        setMemoria("");
+        setTipo_memoria("");
+        setConsumo("");
+        setFecha("");
+        setImagen(null);
+      }
     }
   };
 
@@ -86,7 +184,7 @@ export default function FormGrafica({ grafica_id }) {
       {!isLoadingGrafica && (
         <div className="wrapper fadeInDown">
           <div className="container w-50 border p-4 mt-4 formContent">
-          <h1 className="text-center fadeIn first">
+            <h1 className="text-center fadeIn first">
               {grafica_id ? "Editar gráfica" : "Publicar gráfica"}
             </h1>
             <form
@@ -95,8 +193,11 @@ export default function FormGrafica({ grafica_id }) {
               onSubmit={handleSubmit}
             >
               <div className="col-md-6">
+                {errorNombre && (
+                  <strong className="alert alert-danger">Obligatorio y mas de 3 caracteres</strong>
+                )}
                 <label htmlFor="inputNombre" className="form-label">
-                  Nombre
+                  Nombre*
                 </label>
                 <input
                   type="text"
@@ -104,12 +205,15 @@ export default function FormGrafica({ grafica_id }) {
                   name="nombre"
                   id="inputNombre"
                   value={nombre}
-                  onChange={(e) => setNombre(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorEmpresa && (
+                  <strong className="alert alert-danger">Obligatorio y mas de 3 caracteres</strong>
+                )}
                 <label htmlFor="inputEmpresa" className="form-label">
-                  Empresa
+                  Empresa*
                 </label>
                 <input
                   type="text"
@@ -117,12 +221,15 @@ export default function FormGrafica({ grafica_id }) {
                   name="empresa"
                   id="inputEmpresa"
                   value={empresa}
-                  onChange={(e) => setEmpresa(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorPvpr && (
+                  <strong className="alert alert-danger">Obligatorio, numero, mayor de 0 y maximo 2 decimales</strong>
+                )}
                 <label htmlFor="inputPVPR" className="form-label">
-                  Precio de salida (€)
+                  Precio de salida (€)*
                 </label>
                 <input
                   type="number"
@@ -131,12 +238,15 @@ export default function FormGrafica({ grafica_id }) {
                   id="inputPVPR"
                   step="0.01"
                   value={pvpr}
-                  onChange={(e) => setPvpr(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorArquitectura && (
+                  <strong className="alert alert-danger">Obligatorio y mas de 3 caracteres</strong>
+                )}
                 <label htmlFor="inputArquitectura" className="form-label">
-                  Arquitectura
+                  Arquitectura*
                 </label>
                 <input
                   type="text"
@@ -144,12 +254,15 @@ export default function FormGrafica({ grafica_id }) {
                   name="arquitectura"
                   id="inputArquitectura"
                   value={arquitectura}
-                  onChange={(e) => setArquitectura(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorMemoria && (
+                  <strong className="alert alert-danger">Obligatorio</strong>
+                )}
                 <label htmlFor="inputMemoria" className="form-label">
-                  Memoria (GB)
+                  Memoria (GB)*
                 </label>
                 <input
                   type="number"
@@ -157,12 +270,15 @@ export default function FormGrafica({ grafica_id }) {
                   name="memoria"
                   id="inputMemoria"
                   value={memoria}
-                  onChange={(e) => setMemoria(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorTipo_memoria && (
+                  <strong className="alert alert-danger">Obligatorio</strong>
+                )}
                 <label htmlFor="inputTipo_memoria" className="form-label">
-                  Tipo de memoria
+                  Tipo de memoria*
                 </label>
                 <input
                   type="text"
@@ -170,12 +286,15 @@ export default function FormGrafica({ grafica_id }) {
                   name="tipo_memoria"
                   id="inputTipo_memoria"
                   value={tipo_memoria}
-                  onChange={(e) => setTipo_memoria(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorConsumo && (
+                  <strong className="alert alert-danger">Obligatorio</strong>
+                )}
                 <label htmlFor="inputConsumo" className="form-label">
-                  Consumo (Vatios)
+                  Consumo (Vatios)*
                 </label>
                 <input
                   type="number"
@@ -183,12 +302,15 @@ export default function FormGrafica({ grafica_id }) {
                   name="consumo"
                   id="inputConsumo"
                   value={consumo}
-                  onChange={(e) => setConsumo(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-md-6">
+                {errorFecha && (
+                  <strong className="alert alert-danger">Obligatorio y formato fecha</strong>
+                )}
                 <label htmlFor="inputFecha" className="form-label">
-                  Fecha
+                  Fecha*
                 </label>
                 <input
                   type="date"
@@ -196,27 +318,31 @@ export default function FormGrafica({ grafica_id }) {
                   name="fecha"
                   id="inputFecha"
                   value={fecha}
-                  onChange={(e) => setFecha(e.target.value)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-12">
+                {errorImagen && (
+                  <strong className="alert alert-danger">Obligatorio</strong>
+                )}
                 <label htmlFor="inputImagen" className="form-label">
-                  Imagen
+                  Imagen*
                 </label>
                 <input
                   type="file"
                   className="form-control"
                   name="imagen"
                   id="inputImagen"
-                  onChange={(e) => setImagen(e.target.files[0].name)}
+                  onChange={(e) => handleChange(e.target)}
                 />
               </div>
               <div className="col-12 text-center d-flex">
-              <input
-                  type="submit"
-                  className="fadeIn fourth"
-                  value={grafica_id ? "Editar" : "Publicar"}
-                />
+                {formValido &&
+                  <input
+                    type="submit"
+                    className="fadeIn fourth"
+                    value={grafica_id ? "Editar" : "Publicar"}
+                  />}
                 <Link className="fadeIn fourth" to="/graficas">
                   <input
                     type="button"
